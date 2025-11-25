c818879dc8ac4df86653b9c9e48657f41eaa6ac1
pawat <pchomphoosang@gmail.com>
2025-11-25 23:12:56 +0700

    fix apply dark mode before showing a page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="th">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                try {
+                  const theme = localStorage.getItem('theme');
+                  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+                  
+                  if (theme === 'dark' || (!theme && prefersDark)) {
+                    document.documentElement.classList.add('dark');
+                  }
+                } catch (e) {}
+              })();
+            `,
+          }}
+        />
+      </head>
       <body>
         <AuthProvider>
           <AppModeProvider>
